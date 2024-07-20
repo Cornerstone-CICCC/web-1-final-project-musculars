1ed80eedd6cb11e94bc3a397a85946d4c33cdd06
yuta-hamasaki <yh02052002@gmail.com>
2024-07-19 22:09:36 -0700

    fixed conflict and merged everything

--- script/script.js
+++ script/script.js
@@ -75,11 +75,9 @@ nextbtn.addEventListener("click",(e) =>{
         servicedetailspage.classList.add("showing"); // show second page
         document.querySelector(".secondpageservice").textContent = "Bridal Service";
         forwedding.classList.add("showing");
-
     }else if(selectedtype===eventparty){
         servicedetailspage.classList.add("showing"); // show second page
         forparty.classList.add("showing");
-
     }else{
         thankyoupage.classList.add("showing"); // show final page   
         document.querySelector("#messageforclient").textContent = `Thank you ${clientname}`;
@@ -198,11 +196,11 @@ submitbtn.addEventListener("click",(e) =>{
     
 
 
-// Modal of beauty tips section
 const thumbnails = document.querySelectorAll(".thumbnail");
 const modal = document.getElementById("videoModal");
 const videoFrame = document.getElementById("videoFrame");
 const closeBtn = document.querySelector(".close");
+const openChat = document.getElementById("chat-open");
 
 thumbnails.forEach((thumbnail) => {
   thumbnail.addEventListener("click", () => {
@@ -215,4 +213,68 @@ thumbnails.forEach((thumbnail) => {
 closeBtn.addEventListener("click", () => {
   modal.style.display = "none";
   videoFrame.src = "";
+});
+
+openChat.addEventListener("click", () => {
+const currentSrc = openChat.src.split('/').pop();
+
+if (currentSrc !== "onClickChat.svg") {
+    openChat.src = "./image/onClickChat.svg";
+} else {
+    openChat.src = "./image/chatIcon.png";
+}
+});
+
+
+// My Work ページの切り替え処理
+window.addEventListener('load', () => {
+  // checkbox
+  const checkbox1 = document.getElementById("makeups");
+  const checkbox2 = document.getElementById("hairstyle");
+
+  // label
+  const label1 = document.getElementById("makeupLabel");
+  const label2 = document.getElementById("hairLabel");
+  const underline1 = document.getElementById("underline1")
+  const underline2 = document.getElementById("underline2")
+
+  // タブの内容部分
+  const hoge1Content = document.getElementById("makeup-container");
+  const hoge2Content = document.getElementById("hair-container");
+
+  // Initial state setup
+  checkbox1.checked = true;
+  checkbox2.checked = false;
+  hoge1Content.style.display = "block";
+  hoge2Content.style.display = "none";
+  label1.style.color = "black"
+  label2.style.color = "#888888"
+  underline1.style.backgroundColor = "black"
+  underline2.style.backgroundColor = "#888888"
+
+  // hoge1タブのクリック時
+  label1.addEventListener("click", function () {
+    checkbox1.checked = true;
+    checkbox2.checked = false;
+    hoge1Content.style.display = "block";
+    label1.style.color = "black"
+    label2.style.color = "#888888"
+    hoge2Content.style.display = "none";
+    underline1.style.backgroundColor = "black"
+    underline2.style.backgroundColor = "#888888"
+    checkbox2.checked = false;
+  });
+
+  // hoge2タブのクリック時
+  label2.addEventListener("click", function () {
+    checkbox2.checked = true;
+    checkbox1.checked = false;
+    label1.style.color = "#888888"
+    label2.style.color = "black"
+    underline1.style.backgroundColor = "#888888"
+    underline2.style.backgroundColor = "black"
+    hoge2Content.style.display = "block";
+    hoge1Content.style.display = "none";
+    checkbox1.checked = false;
+  });
 });
