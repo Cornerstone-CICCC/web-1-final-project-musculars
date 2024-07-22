58f441137201c0c043ca2ae14fcc7d44789f068d
Risa Yamamoto <yamari2422@gmail.com>
2024-07-21 19:19:29 -0700

    modified : componentized the header and footer

--- script/script.js
+++ script/script.js
@@ -1,4 +1,13 @@
-//const variables for event type 
+//componentization of the header and footer
+fetch("header.html")
+  .then((response) => response.text())
+  .then((data) => (document.querySelector("#header").innerHTML = data));
+
+fetch("footer.html")
+  .then((response) => response.text())
+  .then((data) => (document.querySelector("#footer").innerHTML = data));
+
+//const variables for event type
 const eventwedding = "eventwedding";
 const eventparty = "eventparty";
 const eventphotoshoot = "eventphotoshoot";
